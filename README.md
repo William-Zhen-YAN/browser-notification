4bfd3dcebb3d8f76876afa46546386544703c74f
Zhen <william.zhen.yan@gmail.com>
2020-08-10 14:13:59 +0900

    first version

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # browser-notification
+
 A lite package to display desktop notification for web application.
+
+## On starting the app
+
+Check if permission has been granted. If not, show a dialogue to ask for user's choice.
+
+example:
+
+```
+const hasPermission = await BrowserNotification.askPermission();
+if (hasPermission) {
+    // do something if permission has been granted.
+} else {
+    // do something to ask for permission
+}
+```
+
+## On showing notification
+
+### 1. Show a small number of notifications
+
+Use this when you have a small number of notifications to show.
+
+example:
+
+```
+BrowserNotification.notify({
+    title: 'title',
+    message: 'message',
+    icon:'path of icon'});
+```
+
+### 2. Show a large number of notifications
+
+Use this when you have a large number of notifications to show.
+
+example:
+
+```
+BrowserNotification.debounceNotify({
+    title: 'title',
+    message: 'message',
+    icon:'path of icon'});
+```
+
+## Constraints
+
+- This will not work in a http environment.
+- Only show the latest notification when a large amount of notifications are received concurrently.
---
+++ src/index.ts
@@ -0,0 +1,90 @@
+import * as _ from "lodash"
+
+export interface BrowserNotificationParam {
+  title: string;
+  message: string;
+  icon?: string;
+}
+
+export default class BrowserNotification {
+  public static askPermission(): Promise<boolean> {
+    const handlePermission = (permission: NotificationPermission) => {
+      // store choice("default" | "denied" | "granted") of user
+      if (!("permission" in Notification)) {
+        (Notification as any).permission = permission;
+      }
+    };
+
+    return new Promise((resolve: any) => {
+      if (!("permission" in window)) {
+        console.warn("Notification is not supported in current browser.");
+        resolve(false);
+      } else {
+        if (this._checkNotificationPromise()) {
+          Notification.requestPermission()
+            .then((permission) => {
+              handlePermission(permission);
+              this._canNotify = permission === "granted";
+              resolve(this._canNotify);
+            })
+            .catch((error) => {
+              console.warn("Request permission failed", error);
+              resolve(false);
+            });
+        } else {
+          Notification.requestPermission((permission) => {
+            handlePermission(permission);
+          })
+            .then((permission) => {
+              this._canNotify = permission === "granted";
+              resolve(this._canNotify);
+            })
+            .catch((error) => {
+              console.warn("Request permission failed", error);
+              resolve(false);
+            });
+        }
+      }
+    });
+  }
+
+  public static notify(param: BrowserNotificationParam) {
+    if (!this._canNotify) {
+      return;
+    }
+    this._currentNotify = new Notification(param.title, {
+      body: param.message,
+      icon: param.icon,
+    });
+  }
+
+  public static debounceNotify(param: BrowserNotificationParam) {
+    if (!this._canNotify) {
+      return;
+    }
+
+    if (!this._debounceNotify) {
+      this._debounceNotify = _.throttle(() => {
+        if (this._currentNotify) {
+          this._currentNotify.close();
+        }
+        this.notify(param);
+      }, 2000);
+    }
+
+    this._debounceNotify(param);
+  }
+
+  private static _canNotify: boolean = false;
+  private static _currentNotify: any = null;
+  private static _debounceNotify: any = null;
+
+  private static _checkNotificationPromise(): boolean {
+    try {
+      Notification.requestPermission().then();
+    } catch (e) {
+      return false;
+    }
+    return true;
+  }
+}
